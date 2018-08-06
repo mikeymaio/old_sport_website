59ed171f1943daa55e36dd4004aa443de0d3c47c
HypeUFO <michael.maio22@gmail.com>
2018-08-05 20:44:23 -0400

    add tabs to tour page

--- src/components/Landing/Tour/TourSlide.js
+++ src/components/Landing/Tour/TourSlide.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import TourList from './TourList';
+import 'react-tabs/style/react-tabs.css';
 
 export default class TourSlide extends Component {
     state = {
@@ -16,6 +18,17 @@ export default class TourSlide extends Component {
       const data = resJson.resultsPage.results.event
       this.setState({ upcomingEvents: data });
     });
+
+    
+    fetch('https://api.songkick.com/api/3.0/artists/8772679/gigography.json?apikey=oVkLSsVWWKzFBEB5')
+    .then((response) => {
+      return response.json();
+    })
+    .then((resJson) => {
+      console.log(resJson.resultsPage);
+      const data = resJson.resultsPage.results.event
+      this.setState({ pastEvents: data });
+    });
   }
 
   render() {
@@ -31,7 +44,22 @@ export default class TourSlide extends Component {
             TOUR
           </div>
           <div className="slide__text slide__text--1">
-            <TourList gigs={this.state.upcomingEvents} />
+            {/* <TourList gigs={this.state.upcomingEvents} /> */}
+
+            <Tabs>
+              <TabList>
+                <Tab className="tab__header">UPCOMING</Tab>
+                <Tab className="tab__header">PAST</Tab>
+              </TabList>
+
+              <TabPanel>
+                <TourList gigs={this.state.upcomingEvents} />
+              </TabPanel>
+              <TabPanel>
+                <TourList gigs={this.state.pastEvents} />
+              </TabPanel>
+            </Tabs>
+
             <div className="sk-logo__wrapper">
               <img src={require('../../../assets/powered-by-sk/powered-by-songkick-white.svg')} style={{width: 100 }}/>
             </div>
