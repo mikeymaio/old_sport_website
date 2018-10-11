9b99493c2387ef0c68fc290cf97ffcc3cebd6756
HypeUFO <michael.maio22@gmail.com>
2018-10-10 17:13:54 -0700

    fix overlay and bio title height

--- src/components/Landing/AboutSlide.js
+++ src/components/Landing/AboutSlide.js
@@ -16,22 +16,26 @@ export default class AboutSlide extends Component {
               BIO
             </div>
           </div>
-          <div className="about__container" style={{ position: 'relative', zIndex: 100, top: 20 }}>
-            <div>
-              <div className="bio-card">
-                <div className="slide__text slide__text--2 title">
+          <div className="about__container" style={{ position: 'relative', zIndex: 100, top: 20, padding: '50px 0' }}>
+            {/* <div> */}
+              <div style={{ position: 'absolute', top: 75 }}>
+              <div className="slide__text slide__text--2 title">
                   Of course we are
                 </div>
                 <div className="slide__text slide__text--2--name title">
                   OLD_SPORT
                 </div>
-                <div className="section__content__wrapper">
-                  <p className="section__content caption">
-                  OLD_SPORT is a five piece soul-rock band out of Los Angeles, California. Formed to create the soundtrack for the award-winning independent film ELSEWHERE, they have since headlined legendary clubs on both coasts, including The Troubadour in L.A. and The Mercury Lounge in Manhattan. They are on the heels of a second successful east coast tour and are currently in the studio recording their debut album with Steve Kille of Dead Meadow. Influences include Spoon, The Strokes, Elvis Costello, Arcade Fire and The E Street Band.
-                  </p>
+              </div>
+              <div style={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <div className="bio-card">
+                  <div className="section__content__wrapper">
+                    <p className="section__content caption">
+                    OLD_SPORT is a five piece soul-rock band out of Los Angeles, California. Formed to create the soundtrack for the award-winning independent film ELSEWHERE, they have since headlined legendary clubs on both coasts, including The Troubadour in L.A. and The Mercury Lounge in Manhattan. They are on the heels of a second successful east coast tour and are currently in the studio recording their debut album with Steve Kille of Dead Meadow. Influences include Spoon, The Strokes, Elvis Costello, Arcade Fire and The E Street Band.
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            {/* </div> */}
           </div>
         </div>
       </div>
